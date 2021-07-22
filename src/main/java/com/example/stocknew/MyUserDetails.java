48a00168f6bb7c290c093d938dea9b2209a0c007
haswanth <vangal.haswanth99@gmail.com>
2021-07-22 13:57:10 +0530

    'added authentication features with spring security'

---
+++ src/main/java/com/example/stocknew/MyUserDetails.java
@@ -0,0 +1,58 @@
+package com.example.stocknew;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.*;
+
+public class MyUserDetails implements UserDetails {
+
+    private String userName;
+    private String password;
+    private Boolean isAdmin;
+    private List<GrantedAuthority> authorities;
+
+    public MyUserDetails(User user) {
+        this.userName = user.getUserName();
+        this.password = user.getPassword();
+        this.authorities=new ArrayList<>();
+        String role=user.isAdmin()? "ROLE_ADMIN":"ROLE_USER";
+        this.authorities.add(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
